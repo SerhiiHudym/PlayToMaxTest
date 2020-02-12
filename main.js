3089af71d7b267287cf68c53b31a7a6bbdf96ac9
Serhii Hudym <serhiihudym@gmail.com>
2020-02-12 22:53:37 +0200

    some addings

--- main.js
+++ main.js
@@ -9,42 +9,107 @@
 
 let game = document.querySelector('#game');
 let table = document.createElement('table');
-let tr = document.createElement('tr');
-let td = document.createElement('td')
-const elements = ['&clubs;', '&hearts;', '&diams;', '&spades;'];
+// let tr = document.createElement('tr');
+// let td = document.createElement('td');
+const elements = ['♣', '♥', '♦', '♠'];
 let rowsNumberInput = document.querySelector('#rowsNumber');
 let columnsNumberInput = document.querySelector('#columnsNumber');
 let createButton = document.querySelector('#createButton');
 
 let rowsNum,
 	colNum,
-	rows = [];
+	// rows = [];
 	playZone = [];
+
+let row = []
 
 createButton.addEventListener('click', () => {
 	rowsNum = rowsNumberInput.value;
 	columnsNum = columnsNumberInput.value;
-	rows = [];
+	// rows = [];
 
 	// for (let i = 0; i < columnsNum; i++) {
 	// 	rows[i] = getRandomInt(4);
 	// }
+	// for (let i in columnsNum) {
+	// 	let symbolsInRow = [];
+
+	// 	for (let j = 0; j < rowsNum; j++) {
+	// 		symbolsInRow[j] = createColumns(columnsNum); // rows
+	// 		playZone[i] = symbolsInRow[j];
+	// 	}
+	// }
+
 
 	for (let i = 0; i < rowsNum; i++) {
-		rows[i] = createColumns(columnsNum); // rows
-		playZone[i] = rows;
+		row = []; // for each row we create a new array
+
+		for (let j = 0; j < columnsNum; j++) {
+			// row = [];
+			row[j] = getRandomInt(4); // for each td in row we create a random value
+		}
+		playZone[i] = row;
+		// row = []
 	}
 
+	console.log(playZone)
+	
 	// for (let i = 0; i < columnsNum; i++) {
 	// 	playZone[i].push(getRandomInt(4));
 	// }
 
-	console.log(playZone);
+	// console.log(rows);
+	createGame();
 });
+
+function createGame() {
+
+	console.log(playZone)
+	for (let i in playZone) {
+		let tr = document.createElement('tr');
+
+		for (let j in playZone[i]) {
+			let td = document.createElement('td')
+			td.innerHTML = playZone[i][j];
+			td.classList.add(`row-${i}`);
+			td.classList.add(`col-${j}`);
+			td.addEventListener('click', (e) => {handleClick(e, i, j)})
+			tr.appendChild(td);
+		}
+		table.appendChild(tr);
+	}
+
+}
+
+function handleClick(event, row, column) {
+	let value = event.target.innerHTML;
+	let target = event.target;
+	target.innerHTML = '';
+
+	setTimeout(() => {
+		const randomElem = getRandomInt(4);
+		target.innerHTML = randomElem;
+		playZone[row][column] = randomElem;
+		console.log(playZone)
+	}, 1000);
+
+	// checkSameElements(value, row, column)
+	
+	
+	// console.log(event.target.innerHTML);
+	// console.log(elements.indexOf(event.target.innerHTML));
+	// console.log(row, column);
+}
+
+function checkSameElements(value, row, column) {
+	for (let i in playZone) {
+		
+	}
+}
 
 function createColumns(num) {
 	for (let i = 0; i < num; i++) {
-		return rows[i] = getRandomInt(4);
+		return getRandomInt(4);
 	}
 }
 
